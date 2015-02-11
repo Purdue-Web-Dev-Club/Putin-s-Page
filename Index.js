685aab787e4b9ddcf32aae3122b962b084ee7871
Merctron <smartomot@hotmail.com>
2015-02-11 15:32:53 -0500

    More code

--- Index.js
+++ Index.js
@@ -1,4 +1,11 @@
 $(document).ready(function () {
-
+	$("#galbutton").click(function () {
+		var overlay = "<div class='gal-overlay' style='z-index:1000; border: 2px solid black;'>";
+            overlay += "<div class='gal-overlay-inner'>";
+            overlay += "</div>";
+            overlay += "</div>";
+            $("body").append(overlay);
+            
+    });
 	
 });
